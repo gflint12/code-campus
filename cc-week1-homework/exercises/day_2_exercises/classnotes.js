a55888610b8ebead1dcd86a4d525595ae0042c0c
Giselle Flint <giselleflint@Giselles-MacBook-Pro.local>
2015-07-08 20:03:42 -0600

    adding javascript exercises for wednesday

---
+++ cc-week1-homework/exercises/day_2_exercises/classnotes.js
@@ -0,0 +1,48 @@
+var num = 0;
+
+// variable number type
+
+var word = "The world";
+
+//variable string type
+
+var isCourtCool = true;
+
+//variable boolean type
+
+var car = {
+    engine: 'V6',
+    make: 'Audi',
+    supercharged: true
+};
+
+//var object type
+
+var startCar = function() {
+
+};
+
+//variable of type function
+
+var num1 = 15;
+var num2 = parseInt('20');
+console.log(num1 + num2);
+
+var aNum = 12.8;
+
+var result = "The Result is: " + ((50 / 13 * aNUm) % 3);
+console.log(result);
+
+
+//adding js to html
+
+<a href = "#" id="click">Click Me</a>
+
+<script type="text/javascript">
+    
+    document getElementById('click').addEventListener('click', funciton(event) {
+        
+    alert('Michael Gentile is an amazing programmer');
+                                                      
+});
+    </script>
