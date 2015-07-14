14cb97335d26df39bd1287c4238e6cb787362ad6
Giselle Flint <giselleflint@Giselles-MacBook-Pro.local>
2015-07-13 19:10:09 -0600

    mario website homework

---
+++ cc-week1-homework/exercises/thurs_exercises/functions.js
@@ -0,0 +1,31 @@
+var numberOne = function(iteration)
+{
+    console.log("ploodle");
+    
+
+}
+
+function numberTwo(iteration) {
+    console.log("polly wanders whanny");
+}
+
+//function three(iteration); {
+//  console.log("calliwhack");
+//    numberFour(iteration)
+//}
+//
+//function four(iteration); {
+//    console.log("cooble");
+//    five(iteration);
+//
+//}
+//
+//function five(iteration); {
+//    iteration ++;
+//    console.log("ploodle");
+//    console.log(iteration);
+//}
+//first(0);
+
+console.log(numberOne());
+console.log(numberTwo());
---
+++ cc-week1-homework/exercises/thurs_exercises/warmup.js
@@ -0,0 +1,10 @@
+for (i=0; i <=100; i++){
+ 
+if (i % 2 ===1) {
+  console.log(i + " " + "Odd");
+}
+
+else {
+  console.log(i + " " + "Even");
+}
+}
